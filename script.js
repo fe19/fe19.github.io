65c295c90583b6140dc4bd6f774ed04e0eb863ea
fe19 <flth741@gmail.com>
2025-03-08 17:14:48 +0100

    Produce generic design description

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var button = document.getElementById('button-generate')
+const button = document.getElementById('button-generate')
+const textFieldGeneration = document.getElementById('text-field-generation-description')
 
 button.addEventListener('click', function () {
-    console.log('Button clicked')
+    const nbrRooms = document.getElementById('input-design-rooms').value;
+    const area = document.getElementById('input-design-area').value;
+    const floor = document.getElementById('input-design-floor').value;
+    textFieldGeneration.textContent = `Generated a design with ${nbrRooms} rooms on ${area} m2 on the ${floor} floor`;
 })
 
 var bsButton = new bootstrap.Button(button)
