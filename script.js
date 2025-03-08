77ac0607bc1a6957fedb734fe1bf5594eaedfe4a
fe19 <flth741@gmail.com>
2025-03-08 16:58:39 +0100

    Introduce JavaScript file

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('button-generate')
+
+button.addEventListener('click', function () {
+    console.log('Button clicked')
+})
+
+var bsButton = new bootstrap.Button(button)
+// bsButton.toggle() //
