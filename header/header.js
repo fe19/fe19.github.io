59c8c6528781f9780bee27b737379da609915a7c
fe <flth741@gmail.com>
2026-05-06 21:35:12 +0200

    move header to own module

---
+++ header/header.js
@@ -0,0 +1,24 @@
+(function () {
+    const script = document.currentScript;
+    const scriptUrl = new URL(script.src, document.baseURI);
+    const base = new URL('..', scriptUrl).href;
+
+    fetch(new URL('header.html', scriptUrl).href)
+        .then(r => r.text())
+        .then(html => {
+            html = html.split('{BASE}').join(base);
+            const target = document.getElementById('site-header');
+            if (target) {
+                target.innerHTML = html;
+            } else {
+                document.body.insertAdjacentHTML('afterbegin', html);
+            }
+            const here = window.location.href.split('#')[0].split('?')[0];
+            document.querySelectorAll('.navbar-nav a.nav-link').forEach(a => {
+                if (a.href.split('#')[0].split('?')[0] === here) {
+                    a.classList.add('active');
+                    a.setAttribute('aria-current', 'page');
+                }
+            });
+        });
+})();
